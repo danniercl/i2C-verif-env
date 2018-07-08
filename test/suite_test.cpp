5b448004ad62c58f73ae86191409bfa8fb6af106
Dannier Castro <dannier.castro@hpe.com>
2018-07-08 13:23:10 -0600

    Agregar los tests de frecuencias @dannier

--- test/suite_test.cpp
+++ test/suite_test.cpp
@@ -3,11 +3,33 @@
 // This is a Basic Write/Read test
 void suite_test::test() {
   intf_int->done = 0;
-  //suite_test::basic_test();
-  //suite_test::random_addr();
-  //suite_test::random_mem_addr();
-  //suite_test::random_data();
-  //suite_test::random_all();
+
+  // B A S I C  T E S T
+  // ------------------
+  suite_test::basic_test();
+
+  // C O R N E R  C A S E S
+  // ----------------------
   suite_test::reset_test();
+
+  // F R E Q U E N C Y
+  // -----------------
+  suite_test::normal_speed();
+  suite_test::fast_speed();
+
+  // R A N D O M  V A L U E S
+  // ------------------------
+  suite_test::random_addr();
+  suite_test::random_mem_addr();
+  suite_test::random_data();
+  suite_test::random_all();
+
   intf_int->done = 1;
+}
+
+void suite_test::clock_counter(){
+  while (true) {
+    wait(1);
+    env->clock_counter++;
+  }
 }
