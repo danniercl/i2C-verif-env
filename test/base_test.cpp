9a8debb49a12b8ac0a850c708b945efdb703b862
Dannier Castro <dannier.castro@hpe.com>
2018-07-07 18:31:55 -0600

    Separar las suites de tests en diferentes archivos @dannier

---
+++ test/base_test.cpp
@@ -0,0 +1,41 @@
+#include "../sc_tb.h"
+
+// This is a Basic Write/Read test
+void base_test::test() {
+   // Generate address (ID) of the slave
+   sc_uint<8> addr = env->drv->stim_gen_inst->addr_rnd_gen(); // Slave address
+   env->drv->reset();
+   sc_uint<8> data = env->drv->stim_gen_inst->data_rnd_gen(); // Generate random data to send
+   cout << "SENT BYTE: " << data << endl;
+
+   sc_uint<8> mem_addr = 0x01;
+
+   // R E S E T
+   // *********
+   intf_int->done = 0;
+   wait(10);
+
+   // S E T  T H E  C O R E
+   // *********************
+   env->drv->core_enable();
+
+   // W R I T E
+   // *********
+   env->drv->write_data(addr, mem_addr, data);
+   wait(1000);
+
+   // R E A D
+   // *******
+   env->drv->read_data(addr, mem_addr);
+   wait(10);
+
+   env->mnt->mnt_out();
+   wait(10);
+   // Request for simulation termination
+   cout << "=======================================" << endl;
+   cout << " SIMULATION END" << endl;
+   cout << "=======================================" << endl;
+   wait(1000);
+   intf_int->done = 1;
+  // Just wait for few cycles
+}
